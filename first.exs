aa64b83e2d1a69fea98258a5cff9192e90a2c043
Jon Rutishauser <jrutishauser@gmail.com>
2017-04-14 00:13:10 -0400

    piping!

--- first.exs
+++ first.exs
@@ -1,8 +1,23 @@
 defmodule First do
-  def total(first, second) do
-    first + second
+  def total(initial) do
+    add_number(initial, 5)
+    |> times_number(3)
+    |> subtract_number(1)
   end
+
+  def add_number(initial, number) do
+    initial + number
+  end
+
+  def times_number(initial, number) do
+    initial * number
+  end
+
+  def subtract_number(initial, number) do
+    initial - number
+  end
+
 end
 
-total_numbers = First.total(10, 20)
+total_numbers = First.total(10)
 IO.puts "total: #{total_numbers}"
