4123c5cda25d0c5920724736f76dcc529c652891
Jon Rutishauser <jrutishauser@gmail.com>
2017-04-18 21:40:02 -0400

    recursion, heads and tails. two cases

--- patterns.exs
+++ patterns.exs
@@ -1,10 +1,14 @@
-"First " <> second = "First Second"
+defmodule Recursion do
+
+  def print_list([head | tail]) do
+    IO.puts head
+    print_list(tail)
+  end
+
+# terminating scenario
+  def print_list([]) do
+  end
+end
 
 inputs = ["First Input", "Second Input", "Third Input"]
-
-[fir, sec, thi] = inputs
-
-
-IO.puts second
-
-IO.puts "#{fir}, #{sec}, #{thi}"
+Recursion.print_list(inputs)
